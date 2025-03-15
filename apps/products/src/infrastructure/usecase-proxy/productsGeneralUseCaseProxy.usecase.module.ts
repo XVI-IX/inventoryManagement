09ea428183958f385b0adb8b126c8f6469709898
David-Daniel Ojebiyi <oladoja14@gmail.com>
2025-03-15 17:25:56 +0100

    feat (productsGeneralUseCasePRoxy) - add product usecases to general usecase proxy

--- apps/products/src/infrastructure/usecase-proxy/productsGeneralUseCaseProxy.usecase.module.ts
+++ apps/products/src/infrastructure/usecase-proxy/productsGeneralUseCaseProxy.usecase.module.ts
@@ -5,11 +5,22 @@ import { CategoryRepository } from '../repositories/category.repository';
 import { UseCaseProxy } from '@app/lib/infrastructure/usecase-proxy/usecase-proxy';
 import { CreateCategoryUseCase } from '../../usecase/Category/createCategory.usecase';
 import { ProductRepository } from '../repositories/product.repository';
+import { GetAllProductsUseCase } from '../../usecase/products/getAllProducts.usecase';
+import { UpdateProductUseCase } from '../../usecase/products/updateProduct.usecase';
+import { CreateProductUseCase } from '../../usecase/products/createProduct.usecase';
+import { DeleteProductUseCase } from '../../usecase/products/deleteProduct.usecase';
 
 @Module({
   imports: [DatabaseModule, ProductRepositoryModule],
 })
 export class ProductGeneralUseCaseModule {
+  // Products usecases
+  static CREATE_PRODUCT_USE_CASE_PROXY = 'CREATE_PRODUCT_USE_CASE_PROXY';
+  static GET_ALL_PRODUCTS_USE_CASE_PROXY = 'GET_ALL_PRODUCTS_USE_CASE_PROXY';
+  static GET_PRODUCT_BY_ID_USE_CASE_PROXY = 'GET_PRODUCT_BY_ID_USE_CASE_PROXY';
+  static UPDATE_PRODUCT_USE_CASE_PROXY = 'UPDATE_PRODUCT_USE_CASE_PROXY';
+  static DELETE_PRODUCT_USE_CASE_PROXY = 'DELETE_PRODUCT_USE_CASE_PROXY';
+
   static CREATE_CATEGORY_USE_CASE_PROXY = 'CREATE_CATEGORY_USE_CASE_PROXY';
 
   static register(): DynamicModule {
@@ -27,8 +38,51 @@ export class ProductGeneralUseCaseModule {
               new CreateCategoryUseCase(categoryRepository, productRepository),
             ),
         },
+        {
+          inject: [ProductRepository],
+          provide: ProductGeneralUseCaseModule.GET_ALL_PRODUCTS_USE_CASE_PROXY,
+          useFactory: (productRepository: ProductRepository) =>
+            new UseCaseProxy(new GetAllProductsUseCase(productRepository)),
+        },
+        {
+          inject: [ProductRepository],
+          provide: ProductGeneralUseCaseModule.GET_PRODUCT_BY_ID_USE_CASE_PROXY,
+          useFactory: (productRepository: ProductRepository) =>
+            new UseCaseProxy(new GetAllProductsUseCase(productRepository)),
+        },
+        {
+          inject: [ProductRepository],
+          provide: ProductGeneralUseCaseModule.UPDATE_PRODUCT_USE_CASE_PROXY,
+          useFactory: (productRepository: ProductRepository) =>
+            new UseCaseProxy(new UpdateProductUseCase(productRepository)),
+        },
+        {
+          inject: [ProductRepository],
+          provide: ProductGeneralUseCaseModule.CREATE_CATEGORY_USE_CASE_PROXY,
+          useFactory: (
+            productRepository: ProductRepository,
+            categoryRepository: CategoryRepository,
+          ) =>
+            new UseCaseProxy(
+              new CreateProductUseCase(productRepository, categoryRepository),
+            ),
+        },
+        {
+          inject: [ProductRepository],
+          provide: ProductGeneralUseCaseModule.DELETE_PRODUCT_USE_CASE_PROXY,
+          useFactory: (productRepository: ProductRepository) =>
+            new UseCaseProxy(new DeleteProductUseCase(productRepository)),
+        },
       ],
-      exports: [ProductGeneralUseCaseModule.CREATE_CATEGORY_USE_CASE_PROXY],
+      exports: [
+        ProductGeneralUseCaseModule.CREATE_CATEGORY_USE_CASE_PROXY,
+
+        ProductGeneralUseCaseModule.GET_ALL_PRODUCTS_USE_CASE_PROXY,
+        ProductGeneralUseCaseModule.GET_PRODUCT_BY_ID_USE_CASE_PROXY,
+        ProductGeneralUseCaseModule.UPDATE_PRODUCT_USE_CASE_PROXY,
+        ProductGeneralUseCaseModule.DELETE_PRODUCT_USE_CASE_PROXY,
+        ProductGeneralUseCaseModule.CREATE_CATEGORY_USE_CASE_PROXY,
+      ],
     };
   }
 }
